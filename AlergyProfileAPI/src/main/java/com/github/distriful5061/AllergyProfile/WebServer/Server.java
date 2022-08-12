d727c78ce9bb3b4cf9761d2481beb81a77836262
distriful5061 <watamituofiical@gmail.com>
2022-08-12 10:50:11 +0900

    Springへ移行するから最後に残しておきます

--- AlergyProfileAPI/src/main/java/com/github/distriful5061/AllergyProfile/WebServer/Server.java
+++
@@ -1,8 +0,0 @@
-package com.github.distriful5061.AllergyProfile.WebServer;
-
-public class Server implements Runnable{
-    @Override
-    public void run() {
-
-    }
-}
---
+++ DeprecatedAllergyProfileAPI/src/main/java/com/github/distriful5061/AllergyProfile/WebServer/Http/HttpServerHost.java
@@ -0,0 +1,4 @@
+package com.github.distriful5061.AllergyProfile.WebServer.Http;
+
+public class HttpServerHost {
+}
