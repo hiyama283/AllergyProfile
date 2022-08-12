15d3d5f656d6ac9111a960fb75d5402d582ec979
distriful5061 <watamituofiical@gmail.com>
2022-08-12 11:02:38 +0900

    Spring Initファイル展開

---
+++ AllergyprofileApi/src/main/java/com/github/distriful5061/allergyprofile/AllergyprofileApi/AllergyprofileApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.distriful5061.allergyprofile.AllergyprofileApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllergyprofileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllergyprofileApiApplication.class, args);
+	}
+
+}
---
+++ AllergyprofileApi/src/test/java/com/github/distriful5061/allergyprofile/AllergyprofileApi/AllergyprofileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.distriful5061.allergyprofile.AllergyprofileApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllergyprofileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
